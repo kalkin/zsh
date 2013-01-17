4ab30260152e03ebfe43e49054706be4b58623e8
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2013-01-17 12:56:05 +0100

    Shorted the git hash

--- kalkinprompt.zsh
+++ kalkinprompt.zsh
@@ -25,7 +25,7 @@ prompt_prepare_git() {
     zstyle ':vcs_info:*' check-for-changes true
     zstyle ':vcs_info:git:*' stagedstr 'Ⓐ '
     zstyle ':vcs_info:(git|hg):*' unstagedstr 'Ⓜ '
-    zstyle ':vcs_info:*' formats '%b@%i-%c%u'
+    zstyle ':vcs_info:*' formats '%b@%8.8i-%c%u'
 }
 
 prompt_construct_first_line(){
