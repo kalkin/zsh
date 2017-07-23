d655a0e6b53c98c95b370361e9f080ebeebed2a0
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-07-23 19:57:54 +0200

    Use perl6 from rakudo-pkg

--- init_path.zsh
+++ init_path.zsh
@@ -9,6 +9,6 @@ prepend_to_path $HOME/.local/bin
 prepend_to_path $HOME/.local/sbin
 prepend_to_path $HOME/bin
 prepend_to_path $XDG_CONFIG_HOME/bin
-prepend_to_path $XDG_DATA_HOME/p6v/perl6/share/perl6/site/bin
-prepend_to_path $XDG_DATA_HOME/p6v/perl6/bin
+prepend_to_path /opt/rakudo/bin
+prepend_to_path $HOME/.perl6/bin
 path=($^path(/FN))  # deduplicate paths
