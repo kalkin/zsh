0a4c5be6c880d9d8f263ee879e47f22a85d87710
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-09-01 21:59:53 +0200

    Move git aliases to own unit

--- aliases.zsh
+++ aliases.zsh
@@ -33,20 +33,6 @@ if (( $+commands[hub] )) ; then
     alias fork="hub fork"
 fi
 
-alias rebase="git rebase"
-alias add="git add -p" 
-alias pull="git pull" 
-alias push="git push" 
-alias gd="git diff" 
-alias stash="git stash"
-alias pop="git stash pop"
-alias drop="git stash drop"
-
-for line in $(git alias|cut -d'.' -f2-|cut -d" " -f1,2|grep -v "alias"); do
-    an_alias=$(echo "$line"|cut -d" " -f1)
-    git_cmd=$(echo "$line"|cut -d" " -f2)
-    alias "$an_alias"="git $git_cmd"
-done
 
 alias tree="tree -FAC"
 
