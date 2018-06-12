c6089bb2a25e873d414ab0b1093c586b7ed82e46
Bahtiar Gadimov <bahtiar@dia-com.de>
2018-06-12 13:16:12 +0200

    Less only use paging mode if needed

--- exports.zsh
+++ exports.zsh
@@ -28,7 +28,7 @@ export LC_TIME="de_DE.utf8"
 export LC_MESSAGES="en_US.utf8"
 export LC_ALL=""
 export PAGER='less';
-export LESS='-R'
+export LESS='-R -F'
 
 HELPDIR=
 for d in /usr/share/zsh/*; do 
