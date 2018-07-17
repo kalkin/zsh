de14fbac5971e4109ce90101dcd52c2ed01fce1f
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2018-07-17 20:11:13 +0200

    Move html suffix opening to units/suffix-alias.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -17,18 +17,6 @@ alias tree="tree -FAC"
 
 alias myip='curl -s http://www.myip.ch/ | grep -o "[[:digit:]].*[[:digit:]]"'
 
-# Suffix aliases
-alias -s pdf=evince
-alias -s ps=evince
-alias -s {flac,wav,wmv,mpg,avi,mpeg,ogm,m4v,mp4,mkv,ogg}='mpv'
-alias -s {jpg,jpeg,xpm,xbm,png,gif}='eog'
-
-if (( $+commands[firefox] )) ; then
-    alias -s html=firefox
-else 
-    alias -s html=$LINKS_BROWSER
-fi
-
 alias htod="fc -inl -1000|grep `today`|strip-ansi-colors|grep TIME_REGEX"
 alias hyes="fc -inl -2000|grep `yesterday`|strip-ansi-colors|grep TIME_REGEX"
 alias iotop="sudo iotop -o"
--- units/suffix-alias.zsh
+++ units/suffix-alias.zsh
@@ -1,11 +1,12 @@
-# Open some files with a default tool
+# Open some files with a default tools
 alias -s tex=$EDITOR
 alias -s php=$EDITOR
 alias -s java=$EDITOR
 alias -s pl=$EDITOR
 alias -s py=$EDITOR
 
-# Suffix aliases
+alias -s html=$BROWSER
+
 alias -s pdf=evince
 alias -s ps=evince
 alias -s {flac,wav,wmv,mpg,avi,mpeg,ogm,m4v,mp4,mkv,ogg}='mpv'
