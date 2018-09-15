fc4a82a00dca039c0821c99257fb9c917e620b71
Bahtiar Gadimov <bahtiar@dia-com.de>
2018-09-15 08:25:13 +0200

    :zsh-config Change PAGER search color is usable with my st(1) colorscheme

--- units/colorful-pager.zsh
+++ units/colorful-pager.zsh
@@ -2,7 +2,7 @@ if (( $+commands[less] )) ; then
     export LESS_TERMCAP_mb=$(tput bold; tput setaf 6)
     export LESS_TERMCAP_md=$(tput bold; tput setaf 1)
     export LESS_TERMCAP_me=$(tput sgr0)
-    export LESS_TERMCAP_so=$(tput bold; tput setaf 3; tput setab 4)
+    export LESS_TERMCAP_so=$(tput bold; tput setaf 5;)
     export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
     export LESS_TERMCAP_us=$(tput setaf 2)
     export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
