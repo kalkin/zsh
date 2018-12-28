aa0d6a7bfad1e54bd51c278a3216ccff2c7f7b98
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2018-12-29 00:29:05 +0100

    :zsh-config Fix site-contrib/monorepo skip non directories

--- units/monorepo.zsh
+++ units/monorepo.zsh
@@ -1,6 +1,6 @@
 if [ -d ~/Projects/ ]; then
     hash -d pj=~/Projects
-    for d in ~pj/*; do
+    for d in $(print -rl -- ~pj/*(/)); do
         name=$(basename "$d")
         hash -d "$name"="$d"
     done
