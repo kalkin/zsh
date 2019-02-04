29270ffdacf4d3cdceb8d076d8a82fd822d9638b
Bahtiar Gadimov <bahtiar@dia-com.de>
2019-02-04 15:53:56 +0100

    :zsh-config Always set EDITOR & [n]vi[m] to some vi like editor

--- units/editor-vi.zsh
+++ units/editor-vi.zsh
@@ -1,23 +1,21 @@
 # Set EDITOR if not set to nvim, fall back to vim, or vi
-if [ -z "$EDITOR" ]; then
-    if (( $+commands[nvim] )) ; then
-        alias vim=nvim
-        alias vi=vim
-        export EDITOR=nvim
-        export VISUAL=nvim
-    elif (( $+commands[vim] )) ; then
-        alias vi=vim
-        export EDITOR=vim
-        export VISUAL=vim
-    elif (( $+commands[vim] )) ; then
-        export EDITOR=vi
-        export VISUAL=vi
+if (( $+commands[nvim] )) ; then
+    alias vim=nvim
+    alias vi=vim
+    export EDITOR=nvim
+    export VISUAL=nvim
+elif (( $+commands[vim] )) ; then
+    alias vi=vim
+    export EDITOR=vim
+    export VISUAL=vim
+elif (( $+commands[vim] )) ; then
+    export EDITOR=vi
+    export VISUAL=vi
 
-    fi
+fi
 
-    if (( $+aliases[vim] )) ; then
-        alias view="$aliases[vim] -R"
-    else
-        alias view="vim -R"
-    fi
+if (( $+aliases[vim] )) ; then
+    alias view="$aliases[vim] -R"
+else
+    alias view="vim -R"
 fi
